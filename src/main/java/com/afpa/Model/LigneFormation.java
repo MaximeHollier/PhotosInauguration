bcd94f74cb1257ec1b6386294eaed58b06d28c0a
MaximeHollier <45227376+MaximeHollier@users.noreply.github.com>
2019-05-29 08:53:42 +0200

    CommitTest

--- src/main/java/com/afpa/Model/LigneFormation.java
+++ src/main/java/com/afpa/Model/LigneFormation.java
@@ -1,4 +1,27 @@
 package com.afpa.Model;
 
 public class LigneFormation {
+    private String region;
+    private String libelle;
+
+    public LigneFormation(String region, String libelle) {
+        this.region = region;
+        this.libelle = libelle;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 }
